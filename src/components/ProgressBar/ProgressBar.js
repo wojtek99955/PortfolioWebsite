0c60226ff38a4ff96112a06b4713e4978cf64026
Wojtek Książek <wojtek99955@gmail.com>
2022-02-25 13:05:53 +0100

    scroll to to show progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,21 +1,32 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {SwitchContext} from '../../App';
-import {BarWrapper, Bar} from './ProgressBarStyle';
+import React, { useState, useEffect, useContext } from 'react';
+import { SwitchContext } from '../../App';
+import { BarWrapper, Bar } from './ProgressBarStyle';
 
 function ProgressBar() {
 
     const [scrollTop, setScrollTop] = useState(0);
+    const [showBar, setShowBar] = useState(false);
 
-    const onScroll = () =>{
+    const onScroll = () => {
         const winScroll = document.documentElement.scrollTop;
         const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        const scrolled = (winScroll/height) * 100;
+        const scrolled = (winScroll / height) * 100;
 
         setScrollTop(scrolled);
     }
 
-    useEffect(()=>{
+    const showProgress = () => {
+        if (window.pageYOffset > 400) {
+            setShowBar(true);
+        } else {
+            setShowBar(false);
+        }
+    };
+
+    useEffect(() => {
         window.addEventListener("scroll", onScroll);
+        window.addEventListener("scroll", showProgress)
+
 
         return () => window.addEventListener("scroll", onScroll);
     }, [])
@@ -23,9 +34,9 @@ function ProgressBar() {
     const context = useContext(SwitchContext);
 
     return (
+
         <BarWrapper>
-            <Bar toggle={context.toggle} scrollTop={scrollTop}></Bar>
-            
+            {showBar ? <Bar toggle={context.toggle} scrollTop={scrollTop}></Bar> : null}
         </BarWrapper>
     )
 }
