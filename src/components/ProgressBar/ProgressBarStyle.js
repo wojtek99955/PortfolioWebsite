1ef7c7794f7581b10ce94146f54b0f8f3a82ce81
Wojtek Książek <wojtek99955@gmail.com>
2022-03-16 11:38:37 +0100

    ProgressBar gradient added

--- src/components/ProgressBar/ProgressBarStyle.js
+++ src/components/ProgressBar/ProgressBarStyle.js
@@ -5,7 +5,8 @@ export const BarWrapper = styled.div`
 `;
 
 export const Bar = styled.div`
-    width: ${({scrollTop}) => scrollTop}%;
+    width: ${({ scrollTop }) => scrollTop}%;
     height: 4px;
-    background-color: ${({theme, toggle}) =>toggle? theme.colors.basePurple : "#0E0E1C"};
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 3%, rgba(112,94,220,1) 50%, rgba(199,192,241,1) 100%);
 `;
