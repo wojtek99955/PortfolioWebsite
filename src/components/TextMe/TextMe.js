adf06d791c6b3818005898d26305d498792f6195
Wojtek Książek <wojtek99955@gmail.com>
2022-03-25 11:48:38 +0100

    TextMe styles update

--- src/components/TextMe/TextMe.js
+++ src/components/TextMe/TextMe.js
@@ -1,22 +1,61 @@
 import {useEffect, useState} from 'react'
 import styled from 'styled-components'
 import { FacebookMessenger } from '@styled-icons/fa-brands/FacebookMessenger'
+import {CloseOutline} from '@styled-icons/evaicons-outline/CloseOutline'
+import {Hand} from '@styled-icons/entypo/Hand';
 
 
 const Container = styled.div`
     position: fixed;
-    top:90%;
+    top:70%;
     left:1rem;
+    display: flex;
+    flex-direction: column;
+    width:300px;
+    height:220px;
+    z-index: 10;
+    background: white;
+    align-items: center;
+    gap: 0.5rem;
+    border-radius: 5px;
+    box-shadow: 0px 11px 53px 0px rgba(48, 49, 57, 0.31);
+
+
+    h2{
+        text-align: center ;
+    }
+
+    a{
+        display: block;
+        background-color: ${({theme}) => theme.colors.basePurple};
+        padding: 0.5rem 1.2rem;
+        text-decoration: none;
+        color: white;
+        border-radius: 5px ;
+    }
 `
 
 const MessengerIcon = styled(FacebookMessenger)`
-    color: blue;
-    width: 3rem;
+    color: white;
+    width: 1.2rem;
+    margin-left:0.5rem;
+`
+
+const CloseIcon = styled(CloseOutline)`
+    cursor: pointer;
+    width:1.5rem;
+    align-self: flex-end ;
+    color: ${({theme}) => theme.colors.grey};
+`
+const HandIcon = styled(Hand)`
+    width:4rem;
+    color: orange;
 `
 
 function TextMe() {
 
-    const [showMess, setShowMess] = useState(false);
+    const [showMess, setShowMess] = useState(true);
+    const handleClose = () => setShowMess(false);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -29,7 +68,10 @@ function TextMe() {
         <>
             {showMess ? (
                 <Container>
-                    <MessengerIcon/>
+                    <CloseIcon onClick={handleClose}/>
+                    <h2>Hi!</h2>
+                    <HandIcon/>
+                    <a href="https://m.me/wojtek.ksiazek.14" target="_blank" rel="noreferrer"> Text Me! <MessengerIcon /></a>
                 </Container>)
                 : null }
         </>
