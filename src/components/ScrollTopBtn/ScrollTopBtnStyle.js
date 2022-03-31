74e93a39087cc35e139ac26cae3302d550da1f86
Wojtek Książek <wojtek99955@gmail.com>
2022-04-01 00:27:27 +0200

    ScrollTop hover animation added

--- src/components/ScrollTopBtn/ScrollTopBtnStyle.js
+++ src/components/ScrollTopBtn/ScrollTopBtnStyle.js
@@ -3,8 +3,8 @@ import { ArrowIosUpwardOutline } from '@styled-icons/evaicons-outline/ArrowIosUp
 
 export const ScrollUpIcon = styled(ArrowIosUpwardOutline)`
     width: 2.5rem;
-    color: white; 
-
+    color: white;
+    position: relative;
 `
 
 export const ScrollUpContainer = styled.div`
@@ -22,8 +22,46 @@ export const ScrollUpContainer = styled.div`
     align-items: center;
     border: 2px solid white;
     transition: background-color 300ms;
+    overflow:hidden;
 
     &:hover{
         background-color: ${({ theme }) => theme.colors.basePurple};
+        animation: scaleAnimation 500ms;
+
+        @keyframes scaleAnimation{
+            from{
+                transform: scale(1);
+            }
+            to{
+                transform: scale(1.02) ;
+            }
+        }
+    }
+    &:hover ${ScrollUpIcon}{
+        animation: arrowAnimationUp 500ms;
+        animation-timing-function: ease ;
+
+        @keyframes arrowAnimationUp{
+        0%{
+            transform: translateY(0);
+        }
+        50%{
+            transform: translateY(-100%);
+            opacity:0;
+        }
+        65%{
+            opacity:0;
+        }
+        66%{
+            transform: translateY(100%);
+        }
+        67%{
+            opacity: 1;
+        }
+        80%{
+            transform: translateY(0);
+        }
+
+    }
     }
 `
