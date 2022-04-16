fb7fd2ecd33b6152313362388dbfdbf57f3a67da
Wojtek Książek <wojtek99955@gmail.com>
2022-04-16 15:37:17 +0200

    NoRoutes styles update

--- src/components/Layouts/NoRoutes/NoRoutes.js
+++ src/components/Layouts/NoRoutes/NoRoutes.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Container = styled.section`
   height: 100vh;
@@ -7,6 +8,11 @@ const Container = styled.section`
   align-items: center;
 `;
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
   h2 {
     font-size: 8rem;
     text-align: center;
@@ -20,12 +26,23 @@ const Wrapper = styled.div`
   }
 `;
 
-export const NoRoutes = (props) => {
+const BackButton = styled(Link)`
+  color: white;
+  background-color: ${({ theme }) => theme.colors.basePurple};
+  text-decoration: none;
+  display: block;
+  padding: 0.8rem 1.5rem;
+  margin-top: 2rem;
+  border-radius: 10px;
+`;
+
+export const NoRoutes = () => {
   return (
     <Container>
       <Wrapper>
         <h2>404</h2>
         <p>Page Not Found</p>
+        <BackButton to="/">Back</BackButton>
       </Wrapper>
     </Container>
   );
