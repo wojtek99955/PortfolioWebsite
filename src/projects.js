896c412b29b8004a6184f224df7b227010b64799
Wojtek Książek <wojtek99955@gmail.com>
2022-06-30 13:50:51 +0200

    project added

--- src/projects.js
+++ src/projects.js
@@ -1,45 +1,46 @@
 const projects = [
-    {
-        src: "/img/screens/projekt-mobster.png",
-        name: "Mobster",
-        technologies: ["HTML", "SASS", "GULP", "JS"],
-        link: "https://wojtek99955.github.io/Mobster/",
-        githubLink: "https://github.com/wojtek99955/Mobster",
-    },
-    {
-        src: "/img/screens/carsharing.png",
-        name: "Carsharing Website",
-        technologies: ["HTML", "SASS", "GULP", "JS"],
-        link: "https://wojtek99955.github.io/carsharing/",
-        githubLink: "https://github.com/wojtek99955/carsharing",
-    },
-    {
-        src: "/img/screens/notatki.png",
-        name: "Notes App",
-        technologies: ["React", "Styled-Components", "Styled-Icons"],
-        link: "https://wojtekk-note-app.netlify.app/",
-        githubLink: "https://github.com/wojtek99955/NoteApp",
-    },
-    {
-        src: "/img/screens/projekt-restauracji.png",
-        name: "Restaurant's Website",
-        technologies: ["HTML", "SASS", "GULP", "JS"],
-        link: "https://wojtek99955.github.io/zentro-restaurant/",
-        githubLink: "https://github.com/wojtek99955/zentro-restaurant",
-    },
-    {
-        src: "/img/screens/weatherapp.png",
-        name: "Weather App",
-        technologies: ["React", "Styled-Components", "Fetch", "MUI"],
-        link: "https://weather-app-wojtek.netlify.app/",
-        githubLink: "https://github.com/wojtek99955/weatherapp",
-    }
-
-]
-
-
-
-
-
+  {
+    src: "/img/screens/fancy-shop-1.png",
+    name: "Fancy Shop",
+    technologies: ["React", "Typescript", "Styled-components"],
+    link: "https://wojtekk-dev-fancy-shop.netlify.app/",
+    githubLink: "https://github.com/wojtek99955/shopping-cart-TS",
+  },
+  {
+    src: "/img/screens/notatki.png",
+    name: "Notes App",
+    technologies: ["React", "Styled-Components", "Styled-Icons"],
+    link: "https://wojtekk-note-app.netlify.app/",
+    githubLink: "https://github.com/wojtek99955/NoteApp",
+  },
+  {
+    src: "/img/screens/projekt-mobster.png",
+    name: "Mobster",
+    technologies: ["HTML", "SASS", "GULP", "JS"],
+    link: "https://wojtek99955.github.io/Mobster/",
+    githubLink: "https://github.com/wojtek99955/Mobster",
+  },
+  {
+    src: "/img/screens/carsharing.png",
+    name: "Carsharing Website",
+    technologies: ["HTML", "SASS", "GULP", "JS"],
+    link: "https://wojtek99955.github.io/carsharing/",
+    githubLink: "https://github.com/wojtek99955/carsharing",
+  },
+  {
+    src: "/img/screens/projekt-restauracji.png",
+    name: "Restaurant's Website",
+    technologies: ["HTML", "SASS", "GULP", "JS"],
+    link: "https://wojtek99955.github.io/zentro-restaurant/",
+    githubLink: "https://github.com/wojtek99955/zentro-restaurant",
+  },
+  {
+    src: "/img/screens/weatherapp.png",
+    name: "Weather App",
+    technologies: ["React", "Styled-Components", "Fetch", "MUI"],
+    link: "https://weather-app-wojtek.netlify.app/",
+    githubLink: "https://github.com/wojtek99955/weatherapp",
+  },
+];
 
 export default projects;
