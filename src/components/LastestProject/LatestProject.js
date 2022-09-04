0ac1a1843680b4cbde45e130052ceabd8d7efb14
Wojtek Książek <wojtek99955@gmail.com>
2022-09-04 11:15:51 +0200

    Title added

--- src/components/LastestProject/LatestProject.js
+++ src/components/LastestProject/LatestProject.js
@@ -1,5 +1,11 @@
+import { SectionTittle } from "../Atoms/SectionTittle";
 const LatestProject = () => {
-  return <div></div>;
+  return (
+    <SectionTittle>
+      <h2>Meet BeFit</h2>
+      <h3>My latest project</h3>
+    </SectionTittle>
+  );
 };
 
 export default LatestProject;
