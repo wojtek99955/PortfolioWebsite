4c91a4aef9b9f6ee686f18c4c57c3bfdb35aeca6
Wojtek Książek <wojtek99955@gmail.com>
2022-09-04 14:30:13 +0200

    styles refactor

---
+++ src/components/LastestProject/LatestProjectStyle.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import projectImg from "../../assets/img/latestProject/befit-laptop.png";
+import { ImDatabase } from "react-icons/im";
+import { FaCalculator } from "react-icons/fa";
+import { MdFastfood } from "react-icons/md";
+
+const Container = styled.section`
+  max-width: 1100px;
+  margin: auto;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  p {
+    line-height: 1.7rem;
+    font-size: ${({ theme }) => theme.fontSize.xs};
+    color: ${({ theme }) => theme.colors.grey};
+    text-align: center;
+    margin-top: 2rem;
+    width: 50%;
+  }
+`;
+const ImageContainer = styled.div`
+  background-image: url(${projectImg});
+  background-size: contain;
+  width: 24rem;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
+
+  height: 30rem;
+  &::before {
+    content: "";
+    background-color: #c7c0f1;
+    width: 20rem;
+    height: 20rem;
+    display: block;
+    border-radius: 50%;
+    position: absolute;
+    z-index: -1;
+    top: 5rem;
+    left: 4rem;
+  }
+`;
+const IconsContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 4rem 0;
+`;
+const DatabaseIcon = styled(ImDatabase)`
+  color: ${({ theme }) => theme.colors.basePurple};
+  font-size: 3rem;
+`;
+const IconWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  span {
+    font-size: 1.2rem;
+  }
+`;
+const CalculatorIcon = styled(FaCalculator)`
+  color: ${({ theme }) => theme.colors.basePurple};
+  font-size: 3rem;
+`;
+
+const FoodIcon = styled(MdFastfood)`
+  color: ${({ theme }) => theme.colors.basePurple};
+  font-size: 3rem;
+`;
